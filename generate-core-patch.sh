dc05cf4c107d5da2eb76417e80f0b9e0455021c5
Wim Leers <wim.leers@acquia.com>
2012-10-26 21:30:16 +0200

    Add shell script to generate Edit core patch, inspired by the one I did for Aloha.

---
+++ generate-core-patch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Arg 1 ($1) should be the path to the Drupal 8 git repo.
+# Arg 2 ($2) should be the comment number.
+#
+# Must be called from the edit-8.x-1.x module's git repo.
+#
+# Sample usage:
+#   $ pwd
+#   ~/Work/edit
+#   $ sh corepatch.sh ~/Work/edit 3
+
+DRUPAL_DIR=$1
+EDIT_DIR=`pwd`
+FILENAME=in_place_editing_for_fields
+COMMENTNR=$2
+
+mkdir $DRUPAL_DIR/core/modules/edit
+
+# Generate the patch for the Edit module.
+cp -R edit.info edit.module css images includes js $DRUPAL_DIR/core/modules/edit/
+rm $DRUPAL_DIR/core/modules/edit/includes/text.inc
+cp text.patch $DRUPAL_DIR/
+cd $DRUPAL_DIR
+git apply text.patch
+git add core/modules/edit
+git add core/modules/field/modules/text/lib/Drupal/text/Plugin/field/formatter
+git diff --staged --binary --patch-with-stat > $EDIT_DIR/$FILENAME-$COMMENTNR.patch
+git commit -m "edit module"
+cd $EDIT_DIR
+
+# Generate the patch for the Aloha Editor integration for Edit.
+cp -R edit_aloha $DRUPAL_DIR/core/modules/edit_aloha/
+cd $DRUPAL_DIR
+git add core/modules/edit_aloha
+git diff --staged --binary --patch-with-stat > $EDIT_DIR/$FILENAME-aloha-integration-$COMMENTNR.patch
+git ci -m "Aloha Editor integration for Edit"
+cd $EDIT_DIR
+
+# Undo these last two commits.
+cd $DRUPAL_DIR
+git reset --hard HEAD^^
+rm -rf $DRUPAL_DIR/core/modules/edit
+rm -rf $DRUPAL_DIR/core/modules/edit_aloha
+cd $EDIT_DIR
