206f2764ab730d81f0d4a265d8ac6edec8dd6a32
J. Renée Beach <splendidnoise@gmail.com>
2012-10-31 19:16:20 -0400

    Removed a stray testing variable.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- js/formwidget.js
+++ js/formwidget.js
@@ -16,7 +16,6 @@
       // Create the form asynchronously.
       // @todo: use a different "factory" depending on editable type.
       Drupal.edit.form.create(this.element, function($editable, $field) {
-        var hi = 'hi';
         $editable
           .addClass('edit-belowoverlay')
           .removeClass('edit-highlighted edit-editable');
