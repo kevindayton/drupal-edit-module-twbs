2ebeaa262722a71ba095299087f92678475a5251
Wim Leers <wim.leers@acquia.com>
2012-11-04 13:06:02 +0100

    Add drupalalohawidget.js, part of the Aloha changes I accidentally included in the commit I did for frega (3b3ccd2649fe974106a4fc4a659a0f769872f8e5).

---
+++ js/createjs/editingWidgets/drupalalohawidget.js
@@ -0,0 +1,18 @@
+(function (jQuery, undefined) {
+  // Consistent namespace.
+  jQuery.widget('Drupal.drupalAlohaWidget', jQuery.Create.alohaWidget, {
+    /**
+     * Implements jQuery UI widget factory.
+     */
+    _init: function() {
+      var widget = this;
+      this.options.activating();
+      Drupal.aloha.init(function() {
+        // Let Create's Aloha widget do the rest.
+        jQuery.Create.editWidget.prototype._init.call(widget);
+      });
+    },
+    _initialize: function() {
+    },
+  });
+})(jQuery);
